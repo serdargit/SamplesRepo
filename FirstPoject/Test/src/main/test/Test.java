afc6ba7d4fd0aabf0f94c59583746e3ba5719bcd
SERDAROZCAKAN <SERDAROZCAKAN@LAPTOP-JHQEJNBE>
2019-02-09 18:39:45 +0300

    change to validaty repository name changes

--- FirstPoject/Test/src/main/test/Test.java
+++ FirstPoject/Test/src/main/test/Test.java
@@ -1,11 +1,18 @@
 package main.test;
 
+import java.util.Date;
+
 public class Test {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test");
 
+		//
+		System.out.println(new Date());
 	}
 
 }
